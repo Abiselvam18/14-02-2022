273bd19148347c123a219888dd3163e51c62a86d
Abinaya <abiselvam080@gmail.com>
2022-03-26 10:21:52 +0530

    Commit seven

---
+++ 14-02-2022/src/com/generalprograms/ProductOfNumbers.java
@@ -0,0 +1,30 @@
+package com.generalprograms;
+
+import java.util.Scanner;
+
+//java program to calculate product of two numbers
+public class ProductOfNumbers {
+
+	public static void main(String[] args) {
+		//declare the variables
+		int num1, num2, product;
+
+		//create scanner class object
+		Scanner scanner=new Scanner(System.in);
+		//read values from keyboard
+		System.out.println("Enter the First value:");
+		num1=scanner.nextInt();
+		System.out.println("Enter the second value:");
+		num2=scanner.nextInt();
+
+		//doing operation
+		product=num1*num2;
+
+		//print output on screen
+		System.out.println("Product:"+product);
+		//close scanner class object
+		scanner.close();
+	}
+
+}
+
